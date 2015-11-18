9b2c992fb2507002594e533cd83873d1fa6c8c01
Stephen <stevekm@users.noreply.github.com>
2015-11-18 13:41:48 -0500

    Update bam_to_BigWig.py

--- scripts/bam_to_BigWig.py
+++ scripts/bam_to_BigWig.py
@@ -2,8 +2,9 @@
 from pybedtools.contrib.bigwig import bam_to_bigwig
 import sys
 
-# called by bam_to_BigWig.sh
-print 'Hello? Yes, this is Python!'
+# print 'Number of arguments:', len(sys.argv), 'arguments.', '\n'
+# print 'Argument List:', str(sys.argv), '\n'
+print 'Python script follows here...'
 ###
 
 print 'Genome is: ', str(sys.argv[1]), '\n'
@@ -12,5 +13,7 @@ print 'Bam input file is: ', str(sys.argv[2]), '\n'
 
 print 'Bigwig output file is: ', str(sys.argv[3]), '\n'
 
+
+# bam_to_bigwig(bam='path/to/bam', genome='mm9', output='path/to/bigwig')
 bam_to_bigwig(bam=str(sys.argv[2]), genome=str(sys.argv[1]), output=str(sys.argv[3]))
 
