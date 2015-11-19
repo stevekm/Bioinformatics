320e42c3267fe9d423bb2249a97ee7fdf5d5e4a5
Stephen <stevekm@users.noreply.github.com>
2015-11-19 16:27:38 -0500

    Update alignment_summary_muti_panel_barplot.R

--- scripts/alignment_summary_muti_panel_barplot.R
+++ scripts/alignment_summary_muti_panel_barplot.R
@@ -4,29 +4,40 @@
 # sample data frame with reads
 ReadsTable<-structure(list(Total.Reads = c(43743601.2, 37888316.7, 44664123.66, 39804329.76, 36708833.46, 49666748.82, 47457348.9, 48866464.92), 
                            Aligned.Reads = c(33114730.74, 28309936.98, 34478776.02, 30168289.32, 27943990.62, 33808528.26, 33249888.36, 37155596.4), 
-                           Deduplicated.alignments = c(32004797.76, 26982520.5, 29728794.42, 28707722.94, 26717704.26, 25559734.98, 22385229.36, 34029148.92)), 
-                      .Names = c("Total.Reads", "Aligned.Reads", "Deduplicated.alignments"), 
+                           De.duplicated.alignments = c(32004797.76, 26982520.5, 29728794.42, 28707722.94, 26717704.26, 25559734.98, 22385229.36, 34029148.92)), 
+                      .Names = c("Total.Reads", "Aligned.Reads", "De.duplicated.alignments"), 
                       row.names = c("Sample1", "Sample2", "Sample3", "Sample4", "Sample5", "Sample6", "Sample7", "Sample8"), 
                       class = "data.frame")
 # calculate the percent alignment
 ReadsTable$PercentAligned=signif(c(ReadsTable$Aligned.Reads / ReadsTable$Total.Reads)*100,digits = 4 )
 # calculate the percent deduplicated
-ReadsTable$PercentDedup=signif(c(ReadsTable$Deduplicated.alignments / ReadsTable$Total.Reads)*100,digits = 4 )
+ReadsTable$PercentDedup=signif(c(ReadsTable$De.duplicated.alignments / ReadsTable$Total.Reads)*100,digits = 4 )
 # calculate the number of unaligned reads
 ReadsTable$UnalignedReads=c(ReadsTable$Total.Reads - ReadsTable$Aligned.Reads)
 #calculate the percent unaligned reads
 ReadsTable$PcntUnalignedReads=signif(c(ReadsTable$UnalignedReads / ReadsTable$Total.Reads)*100,digits=4)
 # calculate the number of duplicated reads
-ReadsTable$Duplicated=c(ReadsTable$Aligned.Reads - ReadsTable$Deduplicated.alignments)
+ReadsTable$Duplicated=c(ReadsTable$Aligned.Reads - ReadsTable$De.duplicated.alignments)
 # calculate the percent of duplicated
 ReadsTable$PercentDup=signif(c(ReadsTable$Duplicated / ReadsTable$Total.Reads)*100,digits = 4 )
 
-
 # save the values to be plotted into a transposed matrix, since thats what the barplot() likes
-Dup_Raw_Reads_Matrix<-t(as.matrix( ReadsTable[,which(colnames(ReadsTable) %in% c("Deduplicated.alignments","Duplicated","UnalignedReads")) ] ))
+# # first just get the columns we want
+Dup_Raw_Reads_df<-ReadsTable[,which(colnames(ReadsTable) %in% c("De.duplicated.alignments","Duplicated","UnalignedReads")) ] 
+# reorder the columns because R is dumb
+Dup_Raw_Reads_df<-Dup_Raw_Reads_df[c("De.duplicated.alignments","Duplicated","UnalignedReads")]
+
+# Dup_Raw_Reads_Matrix<-t(as.matrix( ReadsTable[,which(colnames(ReadsTable) %in% c("De.duplicated.alignments","Duplicated","UnalignedReads")) ] ))
+Dup_Raw_Reads_Matrix<-t(as.matrix(Dup_Raw_Reads_df))
 # # divid the number of reads by 1million
-Dup_Raw_Reads_Matrix<-signif(Dup_Raw_Reads_Matrix/1000000,digits = 3)
-Dup_Pcnt_Reads_Matrix<-t(as.matrix( ReadsTable[,which(colnames(ReadsTable) %in% c("PercentDedup","PercentDup","PcntUnalignedReads")) ] ))
+Dup_Raw_Reads_Matrix<-signif(Dup_Raw_Reads_Matrix/1000000,digits = 4)
+
+# # first just get the columns we want
+Dup_Pcnt_Reads_df<-ReadsTable[,which(colnames(ReadsTable) %in% c("PercentDedup","PercentDup","PcntUnalignedReads")) ]
+# reorder
+Dup_Pcnt_Reads_df<-Dup_Pcnt_Reads_df[c("PercentDedup","PercentDup","PcntUnalignedReads")]
+Dup_Pcnt_Reads_Matrix<-t(as.matrix(Dup_Pcnt_Reads_df))
+# Dup_Pcnt_Reads_Matrix<-t(as.matrix( ReadsTable[,which(colnames(ReadsTable) %in% c("PercentDedup","PercentDup","PcntUnalignedReads")) ] ))
 
 
 #####################
