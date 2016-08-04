6b8cb12f2cd5cd4222123cec1620e90639a31cf4
Stephen K <stevekm@users.noreply.github.com>
2016-08-04 15:12:56 -0400

    add new custom plotting and GO functions

--- clusterProfiler_DiffBind_GO_pipeline.R
+++ clusterProfiler_DiffBind_GO_pipeline.R
@@ -90,3 +90,98 @@ enrichMap(ego,vertex.label.cex=0.7)
 cnetplot(ego, categorySize="pvalue", foldChange=fold_change,vertex.label.cex=0.7)
 cnetplot(ego, categorySize="geneNum", foldChange=fold_change,vertex.label.cex=0.7)
 dev.off()
+
+
+
+# ~~~~~ GO CUSTOM WORKFLOW ~~~~~~~~~ #
+# custom GO functions
+custom_groupGO <- function(gene_list, sub_ontology="BP"){
+	# gene_list: char vector of gene ID's
+	# sub_ontology # One of "MF", "BP", and "CC" subontologies.; Cellular Component, ... ... 
+	
+	# run the group GO analysis
+	ggo <- groupGO(gene     = gene_list,
+								 OrgDb    = org.Hs.eg.db,
+								 ont      = sub_ontology,
+								 level    = 3,
+								 readable = TRUE)
+	return(ggo)
+}
+
+
+custom_enrichGO <- function(gene_list, sub_ontology="BP"){
+	# gene_list: char vector of gene ID's
+	# sub_ontology # One of "MF", "BP", and "CC" subontologies.
+	
+	# run the enrichment GO analysis
+	ego <- enrichGO(gene          = gene_list, # genes to look for enrichment in
+									# universe      = names(GO_genes), # background genes
+									OrgDb         = org.Hs.eg.db,
+									ont           = sub_ontology,
+									pAdjustMethod = "BH",
+									pvalueCutoff  = 0.01,
+									qvalueCutoff  = 0.05, 
+									readable      = TRUE)
+	return(ego)
+}
+
+
+
+get_GO_results <- function(GO_object){
+	# save the results to a new dataframe
+	ggo_results <- GO_object@result
+	
+	# sort by count
+	ggo_results <- ggo_results[with(ggo_results, order(Count,decreasing = TRUE)),] # 
+	return(ggo_results)
+}
+
+
+custom_groupGO_barplot <- function(GO_counts, GO_descriptions, sub_ontology="BP"){
+	# make a vector of values to plot
+	GO_results_vec <- setNames(GO_counts,GO_descriptions)
+	
+	plot_cols <- rainbow(n = length(GO_results_vec))
+	
+	# plot margins # c(bottom, left, top, right) # default is c(5, 4, 4, 2) + 0.1
+	par(mar=c(5, 20, 4, 2) + 0.1)
+	barplot(GO_results_vec,horiz = TRUE,las=1,col = plot_cols,xlab = "Number of genes",cex.names=0.75,main = paste0("Group GO plot: ",sub_ontology))
+}
+
+
+custom_enrichGO_barplot <- function(GO_counts, GO_descriptions, sub_ontology="BP"){
+	# make a vector of values to plot
+	GO_results_vec <- setNames(GO_counts,GO_descriptions)
+	
+	plot_cols <- rainbow(n = length(GO_results_vec))
+	
+	# plot margins # c(bottom, left, top, right) # default is c(5, 4, 4, 2) + 0.1
+	par(mar=c(5, 23, 4, 2) + 0.1)
+	barplot(GO_results_vec,horiz = TRUE,las=1,col = plot_cols,xlab = "Number of genes",cex.names=0.75,main = paste0("Enrichment GO plot: ",sub_ontology))
+}
+
+
+
+# save custom GO barplots to a single PDF
+pdf(file = paste0(out_dir,"/GO_plots5.pdf"),width = 10,height = 10,onefile = TRUE)
+for(sub_ont in c("MF","BP","CC")){
+	# get the subontology category
+	tmp_ont <- sub_ont
+	
+	# run group GO functions
+	tmp_ggo <- custom_groupGO(names(fold_change), tmp_ont)
+	tmp_ggo_results <- get_GO_results(tmp_ggo)
+	# save results to table
+	write.table(x = tmp_ggo_results, file = paste0(out_dir,"/",tmp_ont,"_ggo.tsv"),quote = FALSE,sep = '\t',row.names = FALSE,col.names = TRUE)
+	# plot
+	custom_groupGO_barplot(tmp_ggo_results[["Count"]][1:10],tmp_ggo_results[["Description"]][1:10],tmp_ont)
+	
+	# run enrichment GO functions
+	tmp_ego <- custom_enrichGO(names(fold_change), tmp_ont)
+	tmp_ego_results <- get_GO_results(tmp_ego)
+	# save results to table
+	write.table(x = tmp_ego_results, file = paste0(out_dir,"/",tmp_ont,"_ego.tsv"),quote = FALSE,sep = '\t',row.names = FALSE,col.names = TRUE)
+	# plot
+	custom_enrichGO_barplot(tmp_ego_results[["Count"]][1:10],tmp_ego_results[["Description"]][1:10],tmp_ont)
+}
+dev.off()
