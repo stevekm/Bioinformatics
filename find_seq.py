6b96fb7465287dd9bd0c697453c55ed7040253d2
Stephen K <stevekm@users.noreply.github.com>
2016-08-29 19:51:08 -0400

    Create find_seq.py

---
+++ find_seq.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# python 2.7
+
+# USAGE: find_seq.py <pattern> /path/to/file.fasta
+# example: find_seq.py TAACCACTCA /local/data/iGenomes/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa > test_loc.txt
+
+from Bio import SeqIO
+import re
+import sys
+
+
+# pattern = "TAACCACTCA"
+pattern = sys.argv[1]
+
+# file_path = "/local/data/iGenomes/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa"
+file_path = sys.argv[2]
+
+def search_fasta(pattern, file_path):
+    for record in SeqIO.parse(open(file_path, "rU"), "fasta"):
+        chrom = record.id
+        for match in re.finditer(pattern, str(record.seq)):
+            start_pos = match.start() + 1
+            end_pos = match.end() + 1
+            print chrom, '\t', start_pos, '\t', end_pos
+
+search_fasta(pattern, file_path)
+
