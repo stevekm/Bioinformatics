334d03fc89095ff18fff25ac4bd735f308f5c60b
SteveK <stevekm@users.noreply.github.com>
2016-11-14 17:35:16 -0500

    fix fastqc script params

--- FastQC_Kraken_Krona/FastQC_Kraken_Krona.sh
+++ FastQC_Kraken_Krona/FastQC_Kraken_Krona.sh
@@ -16,19 +16,19 @@ if (($# != 2)); then
   exit
 fi
 
+# get the script arguments
+OUTDIR="$1" # outdir
+mkdir -p "$OUTDIR"
+FASTQ_R1="$2" # Read 1 input file
+
 # ~~~~~~ params ~~~~~~ #
 # these need to be installed on the cluster
 export KRAKEN_DEFAULT_DB="$HOME/ref/Kraken/nt-48G"
 export KRAKEN_NUM_THREADS="$THREADS"
 module load fastqc/0.11.4 # use this if FastQC is not in your $PATH
 
-# get the script arguments
-OUTDIR="$1" # outdir
-mkdir -p "$OUTDIR"
-FASTQ_R1="$2" # Read 1 input file
 
-
-# THREADS is defined by qsub; if not, set with 8 threads
+# THREADS is defined by qsub; if not, set with 2 threads
 THREADS=${NSLOTS:=2} 
 echo "OUTDIR is $OUTDIR"
 echo "FASTQ_R1 is $FASTQ_R1"
@@ -42,8 +42,8 @@ fastqc --threads "$THREADS" --nogroup --outdir "$OUTDIR" "$FASTQ_R1"
 
 # Kraken; check for potential contaminants
 output_file="$(basename $FASTQ_R1)"
-# # only use the first 1,000,000 reads !! # 4,000,000
-zcat $FASTQ_R1 | head -400 | \
+# # only use the first 1,000,000 reads !! # 4000000
+zcat $FASTQ_R1 | head -4000000 | \
 $HOME/software/bin/kraken --fastq-input /dev/fd/0 | \
 tee >(cut -f2,3 > "${OUTDIR}/${output_file}_krona.input") | \
 $HOME/software/bin/kraken-report --show-zeros | awk -F $'\t' '$1>0.1' > "${OUTDIR}/${output_file}_kraken_contaminant_analysis.txt"
