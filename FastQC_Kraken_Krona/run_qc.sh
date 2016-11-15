ef8dc08e5a6244802c0fb4f9eb44e8e06790a8fc
Stephen Kelly <stevekm@users.noreply.github.com>
2016-11-15 12:38:38 -0500

    Update run_qc.sh

--- FastQC_Kraken_Krona/run_qc.sh
+++ FastQC_Kraken_Krona/run_qc.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -x 
 ## USAGE: run.sh /path/to/output_dir /path/to/input_dir /path/to/runscript.sh
-# this script might be broken, fix it later.. 
 # this script will loop over all fastq.gz files it finds and run the script
 # putting the results in an outdir with the same name as the input file
 
@@ -18,11 +17,19 @@ cat $tmp_script
 
 for i in $FILES; do
     tmp_fastq="$(readlink -f $i)"
+    echo -e "File is:\t${tmp_fastq}"
+    
     tmp_outdir="${OUTDIR}/$(basename $tmp_fastq)"
+    mkdir -p "$tmp_outdir"
+    tmp_outdir="$(readlink -f $tmp_outdir)" # need to make sure the full path is being used for qsub sometimes..
+    
     tmp_logdir="${tmp_outdir}/logs"
-    mkdir -p "$tmp_logdir"
-
-    qsub -wd $tmp_outdir -o :${tmp_logdir}/ -e :${tmp_logdir}/ -pe threaded 2-4 "$tmp_script" "$tmp_outdir" "$tmp_fastq"
+    mkdir -p "$tmp_logdir" 
+    tmp_logdir="$(readlink -f $tmp_logdir)"
+    
+    echo -e "Output dir is:\t${tmp_outdir}"
+    
+    qsub -wd $tmp_outdir  -o :${tmp_logdir}/ -e :${tmp_logdir}/ -pe threaded 2-4 "$tmp_script" "$tmp_outdir" "$tmp_fastq" # 
     
     # run without qsub here:
     # "$tmp_script" "$tmp_outdir" "$tmp_fastq" 
