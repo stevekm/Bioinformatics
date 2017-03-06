8fc91f1aa1f783a95b2d1fbb1457b5878c1f95f8
Stephen Kelly <stevekm@users.noreply.github.com>
2017-03-06 17:19:48 -0500

    add ChIPpeakAnno workflow

---
+++ annotate_peaks_ChIPpeakAnno/annotate_peaks.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript
+
+## USAGE: annotate_peaks.R /path/to/peaks.bed /path/to/output/annotated_peaks.tsv
+## DESCRIPTION: This script will run annotate peaks with ChIPpeakAnno, using hg19
+
+# get script args
+args <- commandArgs(TRUE)
+
+cat("\nScript args are:\n")
+print(args)
+
+input_peaks_file <- args[1]
+output_annotated_peaks_file <- args[2]
+
+cat("\nLoading packages...\n")
+
+# source("https://bioconductor.org/biocLite.R")
+# biocLite("ChIPpeakAnno")
+library(ChIPpeakAnno)
+library(biomaRt)
+
+# read in the BED file
+cat("\nReading in the BED file...\n")
+peaks_granges <- toGRanges(input_peaks_file, format="BED", header=FALSE) 
+
+# for hg19
+# get biomart reference genome information
+cat("\nGetting biomart reference information for hg19...\n")
+martEns <- useMart(host="grch37.ensembl.org", biomart="ENSEMBL_MART_ENSEMBL", dataset="hsapiens_gene_ensembl", verbose=F)
+martEnsTSS <- getAnnotation(mart=martEns, featureType="TSS")
+martEnsDF <- getBM(attributes=c("ensembl_gene_id", "external_gene_name", "gene_biotype"), mart=martEns)
+
+
+# get the annotations
+cat("\nGetting annotations...\n")
+peaks_granges <- annotatePeakInBatch(peaks_granges, AnnotationData = martEnsTSS, PeakLocForDistance = "middle", FeatureLocForDistance = "TSS", output = "shortestDistance", multiple = TRUE)
+
+# merge the annotations with the peaks
+cat("\nMerging annotations...\n")
+peaks_granges_df <- merge(as.data.frame(peaks_granges) , martEnsDF , by.x=c("feature"), by.y=c("ensembl_gene_id") , all.x=TRUE)
+
+# save the output
+cat("\nSaving the output...\n")
+write.table(peaks_granges_df, row.names = FALSE, sep = '\t', quote = FALSE, 
+            file = output_annotated_peaks_file)
+
+
---
+++ annotate_peaks_ChIPpeakAnno/run_annotate_peaks.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# this script will find the peaks files in the input directory
+# and run the annotation script on them
+
+project_dir="/ifs/home/kellys04/projects/SmithLab_ChIPSeq_2017-02-11_2/project_notes/peak_annotation_stats"
+input_dir="/ifs/home/kellys04/projects/SmithLab_ChIpSeq_2017-02-11_2/pipeline/peaks/results"
+peaks_file_basename="peaks.bed"
+
+output_dir="${project_dir}/annotated_peaks"
+mkdir -p "$output_dir"
+
+annotation_script="${project_dir}/annotate_peaks.R"
+
+# find all the peaks files to be annotated
+find "$input_dir" -name "$peaks_file_basename" | while read item; do
+set -x
+echo "$item"
+
+input_name="$(echo "$item" | sed -e "s|${input_dir}/||g")"
+sample_ID="$(basename $(dirname "$input_name"))"
+sample_path="$(dirname "$input_name")"
+
+output_path="${output_dir}/${sample_path}"
+log_dir="${output_path}/logs"
+mkdir -p "$log_dir"
+
+output_filepath="${output_path}/${sample_ID}_annotated_peaks.tsv"
+echo "$output_filepath"
+
+echo ""
+qsub -b y -wd "$project_dir" -o :${log_dir}/ -e :${log_dir}/ -N "$sample_ID" Rscript "$annotation_script" "$item" "$output_filepath" # -pe threaded "$job_threads" -l mem_free="$job_mem" -l h_vmem="$job_mem" -l mem_token="$job_mem"
+
+set +x
+done
