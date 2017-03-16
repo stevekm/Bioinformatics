dcb321536d09428bf0a362640f947135fb9184a4
Stephen Kelly <stevekm@users.noreply.github.com>
2017-03-16 13:21:47 -0400

    fix the biomart file save/load in the annotate peaks script

--- annotate_peaks_ChIPpeakAnno/annotate_peaks.R
+++ annotate_peaks_ChIPpeakAnno/annotate_peaks.R
@@ -6,13 +6,13 @@
 # get script args
 args <- commandArgs(TRUE)
 
-cat("\nScript args are:\n")
+message("\nScript args are:\n")
 print(args)
 
 input_peaks_file <- args[1]
 output_annotated_peaks_file <- args[2]
 
-cat("\nLoading packages...\n")
+message("\nLoading packages...\n")
 
 # source("https://bioconductor.org/biocLite.R")
 # biocLite("ChIPpeakAnno")
@@ -20,35 +20,40 @@ library(ChIPpeakAnno)
 library(biomaRt)
 
 # read in the BED file
-cat("\nReading in the BED file...\n")
+message("\nReading in the BED file...\n")
 peaks_granges <- toGRanges(input_peaks_file, format="BED", header=FALSE) 
 
 # for hg19
 # get biomart reference genome information
-# cat("\nGetting biomart reference information for hg19...\n")
-# martEns <- useMart(host="grch37.ensembl.org", biomart="ENSEMBL_MART_ENSEMBL", dataset="hsapiens_gene_ensembl", verbose=F)
-# martEnsTSS <- getAnnotation(mart=martEns, featureType="TSS")
-# martEnsDF <- getBM(attributes=c("ensembl_gene_id", "external_gene_name", "gene_biotype"), mart=martEns)
-# save(martEns, martEnsTSS, martEnsDF, file = "biomart_data.RData")
+# check for a saved copy first..
+biomart_data_file <- file.path(getwd(), "biomart_data.RData")
+if(file.exists(biomart_data_file)){
+    message(sprintf("Found biomaRt data file:\n%s\nLoading data from file...", biomart_data_file))
+    load(biomart_data_file)
+} else {
+    message("Saved biomaRt data file not found!")
+    message("Retreiving reference information for hg19 from biomaRt, this might take a few minutes...")
+    martEns <- useMart(host="grch37.ensembl.org", biomart="ENSEMBL_MART_ENSEMBL", dataset="hsapiens_gene_ensembl", verbose=F)
+    martEnsTSS <- getAnnotation(mart=martEns, featureType="TSS")
+    martEnsDF <- getBM(attributes=c("ensembl_gene_id", "external_gene_name", "gene_biotype"), mart=martEns)
+    message(sprintf("Saving biomaRt data to file:\n%s\n", biomart_data_file))
+    save(martEns, martEnsTSS, martEnsDF, file = biomart_data_file)
+}
 
-cat("\nLoading biomart reference information for hg19...\n")
-load("biomart_data.RData")
 
 # get the annotations
-cat("\nGetting annotations...\n")
+message("\nGetting annotations...\n")
 peaks_granges <- annotatePeakInBatch(peaks_granges, AnnotationData = martEnsTSS, PeakLocForDistance = "middle", FeatureLocForDistance = "TSS", output = "shortestDistance", multiple = TRUE)
 
 # merge the annotations with the peaks
-cat("\nMerging annotations...\n")
+message("\nMerging annotations...\n")
 peaks_granges_df <- merge(as.data.frame(peaks_granges) , martEnsDF , by.x=c("feature"), by.y=c("ensembl_gene_id") , all.x=TRUE)
 
-# !! NOTE: Need to subtract 1 from the 'start' to get the original coordinate !!
-# methyl_granges_df[["start"]] <- as.numeric(methyl_granges_df[["start"]]) - 1
-
 # save the output
-cat("\nSaving the output...\n")
+message("\nSaving the output...\n")
 write.table(peaks_granges_df, row.names = FALSE, sep = '\t', quote = FALSE, 
             file = output_annotated_peaks_file)
 
 
+message("Session Information:\n")
 sessionInfo()
