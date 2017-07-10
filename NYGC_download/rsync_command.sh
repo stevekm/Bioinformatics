58f0eba52404de632a8f5eb4e6233c6adab54b28
Stephen Kelly <stevekm@users.noreply.github.com>
2017-07-10 16:05:50 -0400

    Update rsync_command.sh

--- NYGC_download/rsync_command.sh
+++ NYGC_download/rsync_command.sh
@@ -2,4 +2,9 @@
 set -x
 
 # the command to use for transfer
+
+# first get everything except the huge BAM files
+# rsync -zrtvhP -e ssh username@scp.nygenome.org:/data/delivery/SomeFoundation/data/Project_123 . --exclude="*.bam"
+
+# then get the rest of the files
 rsync -zrtvhP -e ssh username@scp.nygenome.org:/data/delivery/SomeFoundation/data/Project_123 .
