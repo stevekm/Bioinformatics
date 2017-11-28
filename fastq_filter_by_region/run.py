1c7d8175da01b8b124de391f3170ca52f7fe3108
Stephen Kelly <stevekm@users.noreply.github.com>
2017-11-27 22:51:51 -0500

    add fastq_filter_by_region

---
+++ fastq_filter_by_region/run.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Subset a series of fastq.gz files based on reads that fall in desired target regions
+use a .bam file of reads from the fastq's and a .bed file of target regions
+to find which reads fall within the desired regions
+then print those same reads from the original fastq to a new fastq file
+
+
+http://biopython.org/wiki/SeqIO
+http://pysam.readthedocs.io/en/latest/api.html
+
+$ cat sample_files.tsv
+sample	bam	fastq
+HapMap-B17-1267	input_bam/HapMap-B17-1267.bam	input_fastq/HapMap-B17-1267_S8_L002_R2_001.fastq.gz,input_fastq/HapMap-B17-1267_S8_L003_R1_001.fastq.gz,input_fastq/HapMap-B17-1267_S8_L002_R1_001.fastq.gz,input_fastq/HapMap-B17-1267_S8_L001_R1_001.fastq.gz,input_fastq/HapMap-B17-1267_S8_L001_R2_001.fastq.gz,input_fastq/HapMap-B17-1267_S8_L004_R2_001.fastq.gz,input_fastq/HapMap-B17-1267_S8_L004_R1_001.fastq.gz,input_fastq/HapMap-B17-1267_S8_L003_R2_001.fastq.gz
+NTC-H2O	input_bam/NTC-H2O.bam	input_fastq/NTC-H2O_S1_L001_R2_001.fastq.gz,input_fastq/NTC-H2O_S1_L004_R2_001.fastq.gz,input_fastq/NTC-H2O_S1_L002_R1_001.fastq.gz,input_fastq/NTC-H2O_S1_L003_R2_001.fastq.gz,input_fastq/NTC-H2O_S1_L003_R1_001.fastq.gz,input_fastq/NTC-H2O_S1_L004_R1_001.fastq.gz,input_fastq/NTC-H2O_S1_L002_R2_001.fastq.gz,input_fastq/NTC-H2O_S1_L001_R1_001.fastq.gz
+SeraCare-1to1-Positive	input_bam/SeraCare-1to1-Positive.bam	input_fastq/SeraCare-1to1-Positive_S2_L004_R1_001.fastq.gz,input_fastq/SeraCare-1to1-Positive_S2_L003_R2_001.fastq.gz,input_fastq/SeraCare-1to1-Positive_S2_L001_R1_001.fastq.gz,input_fastq/SeraCare-1to1-Positive_S2_L004_R2_001.fastq.gz,input_fastq/SeraCare-1to1-Positive_S2_L002_R2_001.fastq.gz,input_fastq/SeraCare-1to1-Positive_S2_L002_R1_001.fastq.gz,input_fastq/SeraCare-1to1-Positive_S2_L001_R2_001.fastq.gz,input_fastq/SeraCare-1to1-Positive_S2_L003_R1_001.fastq.gz
+"""
+import time
+import os
+import csv
+import gzip
+from Bio import SeqIO
+import pysam
+
+start_time = time.time()
+print('start time: {0}'.format(start_time))
+
+# file with sample IDs, paths to .fastq and .bam files
+samples_file = "sample_files.tsv"
+# file with genomic regions to use
+target_file = "targets.bed"
+# directory to save the output fastq files
+output_dir= "output_fastq"
+
+
+# read in the samples
+samples = []
+with open(samples_file) as f:
+    reader = csv.DictReader(f, delimiter = '\t')
+    for sample in reader:
+        samples.append(sample)
+
+# split the fastq and bam entries
+for sample in samples:
+    sample['fastq'] = sample['fastq'].split(',')
+    sample['bam'] = sample['bam'].split(',')
+    # add 'qname' for later
+    sample['qname'] = []
+    # make fastq output file paths
+    sample['output_fastq'] = {}
+    for fastq in sample['fastq']:
+        sample['output_fastq'][fastq] = os.path.join(output_dir, os.path.basename(fastq)).strip('.gz')
+
+print(samples)
+
+
+
+# read in the target regions
+targets = []
+with open(target_file) as f:
+    for line in f:
+        chrom, start, stop = line.split()
+        targets.append((chrom, int(start), int(stop)))
+
+
+# get the read IDs ('qname') for the targets from each bam per sample
+for chrom, start, stop in targets:
+    print((chrom, start, stop))
+    for sample in samples:
+        for bam_file in sample['bam']:
+            print(bam_file)
+            # load the bam
+            bam = pysam.AlignmentFile(bam_file, "rb")
+            for read in bam.fetch(chrom, start, stop):
+                sample['qname'].append(read.qname)
+            bam.close()
+
+
+# get only the unique qnames per sample
+for sample in samples:
+    sample['qname'] = sorted(set(sample['qname']))
+
+# get the fastq reads that match
+for sample in samples:
+    for fastq in sample['fastq']:
+        output_fastq = sample['output_fastq'][fastq]
+        print((fastq, output_fastq))
+        with gzip.open(fastq) as handle:
+            input_seq_iterator = SeqIO.parse(handle, "fastq")
+            seq_iterator = (record for record in input_seq_iterator if record.seq in sample['qname'])
+            SeqIO.write(seq_iterator, output_fastq, "fastq")
+
+
+end_time = time.time()
+print('end_time: {0}'.format(end_time))
+
+time_elapsed = end_time - start_time
+print('time_elapsed: {0}'.format(time_elapsed))
---
+++ fastq_filter_by_region/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# run the scrip on the HPC cluster
+
+script="run.py"
+
+job_name="filter_fastq"
+
+qsub_logdir="logs"
+mkdir -p "$qsub_logdir"
+
+qsub -wd $PWD -o :${qsub_logdir}/ -e :${qsub_logdir}/ -j y -N "$job_name" <<E0F
+date +"%Y-%m-%d-%H-%M-%S"
+start=\$(date +%s)
+
+module unload python
+module load python/2.7
+
+set -x
+echo "python $script"
+
+date +"%Y-%m-%d-%H-%M-%S"
+echo "Duration: \$(((\$(date +%s)-\$start)/60)) minutes"
+set +x
+E0F
